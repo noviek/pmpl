b5b1f59e18f88326d23935a8a988071849fe7e95
noviek <cs.novie@gmail.com>
2015-09-14 16:00:59 +0000

    Triangulation

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 # from django.http import HttpResponse
 
 def home_page(request):
-	return render(request, 'home.html')
+	#if request.method == 'POST':
+	#	return HttpResponse(request.POST['item_text'])
+	return render(request, 'home.html', {
+		'new_item_text': request.POST.get('item_text', ''),
+	})
 	#return HttpResponse('<html><title>Novie Kamalia - 1206239806</title><body>Hello, Novie Kamalia (1206239806)</body></html>')
 	
