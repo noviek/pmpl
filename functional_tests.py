f031a57ce62de391e74fe64caabcc1943d65a01d
noviek <cs.novie@gmail.com>
2015-09-14 10:27:04 +0000

    Ilangin dikit

--- functional_tests.py
+++ functional_tests.py
@@ -13,11 +13,7 @@ class NewVisitorTest(unittest.TestCase):
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get('http://localhost:8806')
 		self.assertIn('Novie Kamalia - 1206239806', self.browser.title)
-		#self.fail('Finish the test!')
+		self.fail('Finish the test!')
 
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
-
-assert 'To-Do' in browser.title
-
-browser.quit()
