cff69eb17c883767b0c35333a2eb2396c17f7cb6
noviek <cs.novie@gmail.com>
2015-09-23 02:06:48 +0000

    The end of chapter 6

--- lists/urls.py
+++ lists/urls.py
@@ -1,9 +1,7 @@
 #from django.conf.urls import patterns, include, url
-from django.conf.urls import include, url
-from lists import views as list_views
-from lists import urls as list_urls
+from django.conf.urls import url
 #from django.contrib import admin
-
+from lists import views
 
 #urlpatterns = patterns('',
 urlpatterns = [
@@ -12,12 +10,13 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     #url(r'^$', views.home_page, name='home'),
-    url(r'^$', list_views.home_page, name='home'),
     #url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
-    url(r'^lists/', include(list_urls)),
-]
     #url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
     #url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
     
+    url(r'^new$', views.new_list, name='new_list'),
+    url(r'^(\d+)/$', views.view_list, name='view_list'),
+    url(r'^(\d+)/add_item$', views.add_item, name='add_item'),
+]    
     #url(r'^admin/', include(admin.site.urls)),
 #)
