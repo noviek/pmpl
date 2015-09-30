38850e232d1c61d0e4ed0907388664fa6cd4f84d
noviek <cs.novie@gmail.com>
2015-09-30 10:45:38 +0000

    first step of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,17 +1,12 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-#import unittest
 
-#class NewVisitorTest(unittest.TestCase):
 class NewVisitorTest(LiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
-
-	#def tearDown(self):
-	#	self.browser.quit()
 
 	def check_for_row_in_list_table(self, row_text):
 		table = self.browser.find_element_by_id('id_list_table')
@@ -19,7 +14,6 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertIn(row_text, [row.text for row in rows])
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
-		#self.browser.get('http://localhost:8806')
 		self.browser.get(self.live_server_url)
 
 		self.assertIn('Novie Kamalia', self.browser.title)
@@ -35,18 +29,13 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox.send_keys('Buy peacock feathers')
 
 		inputbox.send_keys(Keys.ENTER)
-		novie_list_url = self.browser.current_url #
-		self.assertRegex(novie_list_url, '/lists/.+') #
+		novie_list_url = self.browser.current_url 
+		self.assertRegex(novie_list_url, '/lists/.+') 
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
-
-		#table = self.browser.find_element_by_id('id_list_table')
-		#rows = table.find_elements_by_tag_name('tr')
-		#self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		#self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
@@ -71,7 +60,21 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertIn('Buy milk', page_text)
 
-		#self.fail('Finish the test!')
+	def test_layout_and_styling(self):
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
 
-#if __name__ == '__main__':
-#	unittest.main(warnings='ignore')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=5
+		)
+
+		inputbox.send_keys('testing\n')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=5
+		)
