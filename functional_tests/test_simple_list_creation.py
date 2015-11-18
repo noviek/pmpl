c011e0c6c7c7ccfcbfc208ff006fc7df8560ca5f
noviek <cs.novie@gmail.com>
2015-11-18 06:15:31 +0000

    Moved Fts into their own individual files

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,37 +1,10 @@
-#from django.test import LiveServerTestCase
-import sys
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from .base import FunctionalTest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-#class NewVisitorTest(LiveServerTestCase):
-class NewVisitorTest(StaticLiveServerTestCase):
-
-	@classmethod
-	def setUpClass(cls):
-		for arg in sys.argv:
-			if 'liveserver' in arg:
-				cls.server_url = 'http://' + arg.split('=')[1]
-				return
-		super().setUpClass()
-		cls.server_url = cls.live_server_url
-
-	@classmethod
-	def tearDownClass(cls):
-		if cls.server_url == cls.live_server_url:
-			super().tearDownClass()
-
-	def setUp(self):
-		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(3)
-
-	def check_for_row_in_list_table(self, row_text):
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(row_text, [row.text for row in rows])
+class NewVisitorTest(FunctionalTest):
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
-		#self.browser.get(self.live_server_url)
 		self.browser.get(self.server_url)
 
 		self.assertIn('Novie Kamalia', self.browser.title)
@@ -61,7 +34,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.browser.quit()
 		self.browser = webdriver.Firefox()
 
-		#self.browser.get(self.live_server_url)
 		self.browser.get(self.server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
@@ -78,23 +50,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertIn('Buy milk', page_text)
-
-	def test_layout_and_styling(self):
-		#self.browser.get(self.live_server_url)
-		self.browser.get(self.server_url)
-		self.browser.set_window_size(1024, 768)
-
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertAlmostEqual(
-			inputbox.location['x'] + inputbox.size['width'] / 2,
-			512,
-			delta=5
-		)
-
-		inputbox.send_keys('testing\n')
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertAlmostEqual(
-			inputbox.location['x'] + inputbox.size['width'] / 2,
-			512,
-			delta=5
-		)
