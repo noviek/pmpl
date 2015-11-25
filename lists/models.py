12e87ec52af8d3eedc922e0718eacc29a8b25436
noviek <cs.novie@gmail.com>
2015-11-25 07:12:16 +0000

    ulang dulu

--- lists/models.py
+++ lists/models.py
@@ -8,7 +8,3 @@ class List(models.Model):
 class Item(models.Model):
 	text = models.TextField(default='')
 	list = models.ForeignKey(List, default=None)
-
-	class Meta:
-		ordering = ('id',)
-		unique_together = ('list', 'text')
